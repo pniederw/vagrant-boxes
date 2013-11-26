bbc590b9009247de281e302ef8e3c0f5203e112d
Peter Niederwieser <pniederw@gmail.com>
2013-11-26 17:19:00 +0100

    no need to reboot anymore

--- packer/ubuntu-13.10-server-amd64/scripts/kernel.sh
+++ packer/ubuntu-13.10-server-amd64/scripts/kernel.sh
@@ -5,6 +5,6 @@
 #apt-get install -y linux-image-generic-lts-raring linux-headers-generic-lts-raring
 
 # reboot
-echo "Rebooting the machine..."
-reboot
-sleep 60
+#echo "Rebooting the machine..."
+#reboot
+#sleep 60
