d5774d793a4ac7f6721c3c14d4820dd1394bdf1c
Peter Niederwieser <pniederw@gmail.com>
2013-11-26 17:13:21 +0100

    don't install a backported kernel (especially not the wrong version of it)

--- packer/ubuntu-13.04-server-amd64/scripts/kernel.sh
+++ packer/ubuntu-13.04-server-amd64/scripts/kernel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # install the backported kernel
-apt-get update
-apt-get install -y linux-image-generic-lts-raring linux-headers-generic-lts-raring
+#apt-get update
+#apt-get install -y linux-image-generic-lts-raring linux-headers-generic-lts-raring
 
 # reboot
 echo "Rebooting the machine..."
